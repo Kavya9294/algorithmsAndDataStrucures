141db698077bf4797ce121ec075facf0c8b42ef6
Kavya9294 <kavya.ub9294@gmail.com>
2022-07-26 19:42:23 -0700

    https://leetcode.com/problems/subsets/ and modified subsets to unanimize

--- subset.java
+++ subset.java
@@ -1,21 +1,17 @@
 class Solution {
     List<List<Integer>> result = new ArrayList<>();
-    ArrayList<Integer> subset = new ArrayList<>();
     public List<List<Integer>> subsets(int[] nums) {
-        dfs(0, nums);
+        dfs(0, nums, new ArrayList<>());
         return result;
     }
     
-    public void dfs(int i, int[] nums){
-        if(i >= nums.length){
-            result.add((ArrayList)subset.clone());
-            return;
+    public void dfs(int start, int[] nums, List<Integer> tempArray){
+        result.add(new ArrayList<>(tempArray));
+        for(int i=start;i<nums.length;i++){
+            tempArray.add(nums[i]);
+            dfs(i+1, nums, tempArray);
+            tempArray.remove(tempArray.size()-1);
         }
-        
-        subset.add(nums[i]);
-        dfs(i+1, nums);
-        subset.remove(subset.size()-1);
-        dfs(i+1, nums);
     }
         
 }
