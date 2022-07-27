521c6fb2833e7cee16a249d5cef698db6ade1624
Kavya9294 <kavya.ub9294@gmail.com>
2022-07-26 20:23:29 -0700

    https://leetcode.com/problems/coin-change/ Made it to the same backtrack combo

--- coinChange.java
+++ coinChange.java
@@ -1,31 +1,28 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int[] memo =  new int[amount];
-        int res = helper(coins, amount, 0, memo);
+        int res = helper(coins, amount, memo);
         return res == Integer.MAX_VALUE? -1 : res;
     }
     
-    private int helper(int[] coins, int amount, int total, int[] memo){
-        if(total!= 0 && total < amount && memo[total-1]!= 0 ){
-            return memo[total-1];
-        }
-        if(total == amount){
-            return 0;
-        }
-        if(total > amount){
+    private int helper(int[] coins, int remain, int[] memo){
+        if(remain < 0){
             return Integer.MAX_VALUE;
         }
-        int minSteps = Integer.MAX_VALUE;
-        
-        for(int j = 0;j< coins.length;j++){
-            if(amount<coins[j])
-                continue;
-            int res=helper(coins, amount, coins[j]+total, memo);
-            minSteps = Math.min(minSteps, res == Integer.MAX_VALUE? Integer.MAX_VALUE : 1+ res );
+        if(remain == 0){
+            return 0;
         }
-        if(total != 0 && total < amount)
-            memo[total-1]= minSteps;
-                                
-        return minSteps;
+        if(memo[remain-1] > 0){
+            return memo[remain-1];
+        }
+        int res = Integer.MAX_VALUE;
+        for(int i=0;i<coins.length;i++){
+            int tmp = helper(coins, remain-coins[i],memo);
+            if(tmp != Integer.MAX_VALUE)
+                res = Math.min(res, 1+tmp);
+        }
+        
+        memo[remain-1] =  res;
+        return res;
     }
 }
